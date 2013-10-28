9a8ce535045e3ae873ea6ad71fa4ffd5cb1b7de5
avil13 <13.avil@gmail.com>
2013-10-28 11:54:12 +0300

    Была проблема с обработкой множества элементов

--- numeric_format.js
+++ numeric_format.js
@@ -17,35 +17,43 @@ jQuery(document).ready(function($) {
         }, options);
 
 
-        var str = $(this).html().toString();
 
-        var val = str.replace(/[^\d\.\,]/g, "");
+        var Do = function(el) {
+
+            var str = $(el).html().toString();
+
+            var val = str.replace(/[^\d\.\,]/g, "");
 
 
+            var res = val.toString();
+            var lZero = (val < 0); // Признак отрицательного числа
 
-        var res = val.toString();
-        var lZero = (val < 0); // Признак отрицательного числа
+            // Определение длины форматируемой части
+            var fLen = res.lastIndexOf('.'); // До десятичной точки
+            fLen = (fLen > -1) ? fLen : res.length;
 
-        // Определение длины форматируемой части
-        var fLen = res.lastIndexOf('.'); // До десятичной точки
-        fLen = (fLen > -1) ? fLen : res.length;
+            // Выделение временного буфера
+            settings.tmpRes = res.substring(fLen);
+            var cnt = -1;
+            for (var ind = fLen; ind > 0; ind--) {
+                cnt++; // Формируем временный буфер
 
-        // Выделение временного буфера
-        settings.tmpRes = res.substring(fLen);
-        var cnt = -1;
-        for (var ind = fLen; ind > 0; ind--) {
-            cnt++; // Формируем временный буфер
+                if (((cnt % 3) === 0) && (ind !== fLen) && (!lZero || (ind > 1))) {
+                    settings.tmpRes = settings.thSep + settings.tmpRes;
+                }
 
-            if (((cnt % 3) === 0) && (ind !== fLen) && (!lZero || (ind > 1))) {
-                settings.tmpRes = settings.thSep + settings.tmpRes;
+                settings.tmpRes = res.charAt(ind - 1) + settings.tmpRes;
             }
 
-            settings.tmpRes = res.charAt(ind - 1) + settings.tmpRes;
+            var r = str.replace(new RegExp(val, 'g'), settings.tmpRes.replace('.', settings.dcSep));
+
+            $(el).html(r);
+        };
+
+
+        for (var i = this.length - 1; i >= 0; i--) {
+            Do(this[i]);
         }
 
-
-        var r = str.replace(new RegExp(val, 'g'), settings.tmpRes.replace('.', settings.dcSep));
-
-        $(this).html(r);
     };
 })(jQuery);
